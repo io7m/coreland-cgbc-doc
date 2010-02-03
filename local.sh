3aec239ce1cc9899cee1ecd6894bdafaf7bb14cc
m0 <m0@viper>
2010-02-03 14:49:56 +0000

    Add SUPPORTED fetch

--- local.sh
+++ local.sh
@@ -2,6 +2,8 @@
 
 CGBC_SOURCE="/home/m0/git/coreland/cgbc"
 
+cp "${CGBC_SOURCE}/SUPPORTED" "meta/supported" || exit 1
+
 ./local-mk-spec.sh "${CGBC_SOURCE}/cgbc-bounded_stacks.ads" > "src/cgbc_bounded_stacks.txt" || exit 1
 ./local-mk-spec.sh "${CGBC_SOURCE}/cgbc-bounded_hashed_maps.ads" > "src/cgbc_bounded_hashed_maps.txt" || exit 1
 ./local-mk-spec.sh "${CGBC_SOURCE}/cgbc-bounded_generic_strings.ads" > "src/cgbc_bounded_generic_strings.txt" || exit 1
